774985ef07eebe02c9f481189b9ce9e1550e20e6
Error Lover <soherito@proton.me>
2025-10-22 16:24:40 +0200

    Add create product endpoint to ProductController

--- src/main/java/pl/edu/vistula/firstrestapi/FirstRestApiApplication.java
+++ src/main/java/pl/edu/vistula/firstrestapi/FirstRestApiApplication.java
@@ -2,7 +2,7 @@ package pl.edu.vistula.firstrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(info= @Info(title = "Products api for vistula", version = "1.0",description = "products information"))
 @SpringBootApplication
 public class FirstRestApiApplication {
 
