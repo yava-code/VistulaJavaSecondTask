40ab5abca641155085689ed07d1aa10959385742
Error Lover <soherito@proton.me>
2025-10-22 14:27:53 +0200

    Add create product endpoint to ProductController

--- src/main/java/pl/edu/vistula/firstrestapi/product/api/ProductController.java
+++ src/main/java/pl/edu/vistula/firstrestapi/product/api/ProductController.java
@@ -1,4 +1,25 @@
 package pl.edu.vistula.firstrestapi.product.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.edu.vistula.firstrestapi.product.api.request.ProductRequest;
+import pl.edu.vistula.firstrestapi.product.api.response.ProductResponse;
+import pl.edu.vistula.firstrestapi.product.api.service.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {this.productService = productService;}
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> create (@RequestBody ProductRequest productRequest) {
+        ProductResponse productResponse = productService.create(productRequest);
+        return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
+    }
 }
