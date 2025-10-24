b52117dfcb7d54bfea37e70aa5d11fad2646c50e
Error Lover <soherito@proton.me>
2025-10-25 00:40:21 +0200

    Add JPA support for Product entity and configure H2 database

--- src/main/java/pl/edu/vistula/firstrestapi/product/domain/Product.java
+++ src/main/java/pl/edu/vistula/firstrestapi/product/domain/Product.java
@@ -1,6 +1,14 @@
 package pl.edu.vistula.firstrestapi.product.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue()
     private Long id;
 
     private String name;
--- src/main/java/pl/edu/vistula/firstrestapi/product/repository/ProductRepository.java
+++ src/main/java/pl/edu/vistula/firstrestapi/product/repository/ProductRepository.java
@@ -1,40 +1,8 @@
 package pl.edu.vistula.firstrestapi.product.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.edu.vistula.firstrestapi.product.domain.Product;
 
-import java.util.*;
+public interface ProductRepository extends JpaRepository<Product,Long> {
 
-@Repository
-public class ProductRepository {
-
-    protected final Map<Long, Product> map =new HashMap<>();
-
-    protected long counter = 1;
-
-    public Product save(Product entity) {
-        setId(entity);
-        return entity;
-    }
-    private Product setId(Product entity) {
-        if (entity.getId() != null) {
-            map.put(entity.getId(), entity);
-        } else {
-            entity.setId(counter);
-            map.put(counter, entity);
-            counter++;
-        }
-        return entity;
-    }
-
-    public Optional<Product> findById(Long id) {
-        return Optional.ofNullable(map.get(id));
-    }
-
-    public List<Product> findAll() {
-        return new ArrayList<>(map.values());
-    }
-    public void deleteById(Long id) {
-        map.remove(id);
-    }
 }
